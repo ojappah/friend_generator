f46c790f4d1b26f9d6bedf8d6cc5fad80088d5ec
generous-vector-9581 <ojappah@gmail.com>
2019-10-29 21:17:42 +0000

    Adding friend selection

--- lib/friend_generator.rb
+++ lib/friend_generator.rb
@@ -1,7 +1,2 @@
 require_relative "./friend_generator/version"
-
-#module FriendGenerator
-#  class Error < StandardError; end
-  # Your code goes here...
-#end
 require_relative "./friend_generator/cli"
--- lib/friend_generator/cli.rb
+++ lib/friend_generator/cli.rb
@@ -1,8 +1,23 @@
 #controller
 class FriendGenerator::CLI
   def call
-    puts "Welcome to Friend Generator"
+    friend_options
+    friend_selection
   end
 
+def friend_options
+  puts "Welcome to Friend Generator. What type of Friend are you looking for today?"
+  puts <<-DOC.gsub /^\s*/, ''
+    1. Male
+    2. Female
+    3. Random
+  DOC
+end
 
+
+def friend_selection
+puts "Enter a number from the options above to generate a new friend or type my friends to see a list of your current friends. Type exit to leave the app."
+  input = nil
+  input = gets.strip.downcase
+end
 end
