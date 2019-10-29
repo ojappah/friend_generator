3c462b1885fba3d199a5aff09c71151e36e549b6
generous-vector-9581 <ojappah@gmail.com>
2019-10-29 16:23:50 +0000

    first commit

---
+++ lib/friend_generator.rb
@@ -0,0 +1,6 @@
+require "friend_generator/version"
+
+module FriendGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/friend_generator/version.rb
@@ -0,0 +1,3 @@
+module FriendGenerator
+  VERSION = "0.1.0"
+end
