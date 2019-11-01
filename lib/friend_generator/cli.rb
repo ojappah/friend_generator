97d9973506255f11281eb8bf7b1813b7112c1788
generous-vector-9581 <ojappah@gmail.com>
2019-11-01 01:50:24 +0000

    clean up code

--- lib/friend_generator/cli.rb
+++ lib/friend_generator/cli.rb
@@ -1,51 +1,46 @@
-#controller
 class FriendGenerator::CLI
   def call
     friend_options
     friend_selection
-    #my_friends
     exit
   end
 
-def friend_options
-  puts "Welcome to Friend Generator. What type of Friend are you looking for today?"
-  puts <<-DOC.gsub /^\s*/, ''
+  def friend_options
+    puts "Welcome to Friend Generator. What type of Friend are you looking for today?"
+    puts <<-DOC.gsub /^\s*/, ''
     1. Male
     2. Female
     3. Random
-  DOC
-  @friends = FriendGenerator::Friend.all
-end
-
-
-def friend_selection
-  input = nil
-  while input != "exit"
-    puts "Please enter a number from the options above to generate a new friend or type my friends to see a list of your current friends. Type 'exit' to leave the app."
-    input = gets.strip.downcase
-    case input
-    when "1"
-
-      puts "Congratulations! You have a new Male Friend. Type my friends to see details about your new friend."
-      male_friend
-    when "2"
-      puts "Congratulations! You have a new Female Friend.Type my friends to see details about your new friend."
-      female_friend
-    when "3"
-      puts "Congratulations! You have a new randomly generated Friend.Type my friends to see details about your new friend."
-      random_friend
-    when "my friends"
-      puts "Here is a list of your new friends."
-      #my_friends
-    else
-      puts "That is not a valid option. Please try again. Select from the options given below."
-
-      friend_options
+    DOC
+    @friends = FriendGenerator::Friend.all
   end
-end
-end
 
-def exit
-  puts "Thank you for using Friend Generator. Come back soon to generate more friends."
-end
+
+  def friend_selection
+    input = nil
+    while input != "exit"
+      puts "Please enter a number from the options above to generate a new friend or type my friends to see a list of your current friends. Type 'exit' to leave the app."
+      input = gets.strip.downcase
+      case input
+        when "1"
+          puts "Congratulations! You have a new friend name #{male_friend[:name]}. He is #{male_friend[:age]} years old. Type 'my friends' to see more details about your new friend."
+          male_friend
+        when "2"
+          puts "Congratulations! You have a new friend name #{female_friend[:name]}. She is #{female_friend[:age]} years old. Type 'my friends' to see more details about your new friend."
+          female_friend
+        when "3"
+          puts "Congratulations! You have a new random friend name #{random_friend[:name]}. Your friend is #{random_friend[:age]} years old. Type 'my friends' to see more details about your new friend."
+
+        when "my friends"
+          puts "Here is a list of all your new friends."
+          @@all
+        when "exit"
+          puts "Thank you for using Friend Generator. Come back soon to generate more friends."
+        else
+          puts "That is not a valid option. Please try again. Select from the options given below."
+        friend_options
+      end
+    end
+  end
+
 end
--- lib/friend_generator/friend.rb
+++ lib/friend_generator/friend.rb
@@ -1,68 +1,71 @@
 class FriendGenerator::Friend
 
-attr_accessor :name, :address, :phone, :birthday, :age
+  attr_accessor :name, :address, :phone, :birthday, :age
 
-  @friends = []
-def self.all
-  self.scrape_male_friends
-  self.scrape_female_friends
-  self.scrape_random_friends
-end
+  @@all = []
+  def self.all
+    self.scrape_male_friends
+    self.scrape_female_friends
+    self.scrape_random_friends
+  end
 
-def self.scrape_male_friends
-  @male_friends = []
-  self.scrape_male
-  @friends << self.scrape_male_friends
-end
+  def self.scrape_male_friends
+    @male_friends = []
+    self.scrape_male
+    @male_friends
+  end
 
-def self.scrape_female_friends
-  @female_friends = []
-  self.scrape_female
-  @friends << self.scrape_female_friends
-end
+  def self.scrape_female_friends
+    @female_friends = []
+    self.scrape_female
+    @female_friends
+  end
 
-def self.scrape_random_friends
-  @random_friends = []
-  self.scrape_random
-  @friends << self.scrape_random_friends
-end
+  def self.scrape_random_friends
+    @random_friends = []
+    self.scrape_random
+    @random_friends
+  end
 
-def self.scrape_male
-html = open("https://www.fakenamegenerator.com/gen-male-us-us.php")
-doc = Nokogiri::HTML.parse(html)
-friend_data = doc.css("div.info.content")
-friend_data.each do |data|
-  male_friend = Hash.new
-  male_friend[:name] = friend_data.css("div.address.h3")
-  male_friend[:address] = friend_data.css("div.adr")
-  male_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
-  male_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
-  male_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
-end
+  def self.scrape_male
+    html = open("https://www.fakenamegenerator.com/gen-male-us-us.php")
+    doc = Nokogiri::HTML.parse(html)
+    friend_data = doc.css("div.info.content")
+    friend_data.each do |data|
+    male_friend = Hash.new
+    male_friend[:name] = friend_data.css("div.address.h3")
+    male_friend[:address] = friend_data.css("div.adr")
+    male_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
+    male_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
+    male_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
+  end
 
-def self.scrape_female
-html = open("https://www.fakenamegenerator.com/gen-female-us-us.php")
-doc = Nokogiri::HTML.parse(html)
-friend_data = doc.css("div.info.content")
-friend_data.each do |data|
-  female_friend = Hash.new
-  female_friend[:name] = friend_data.css("div.address.h3")
-  female_friend[:address] = friend_data.css("div.adr")
-  female_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
-  female_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
-  female_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
-end
+  def self.scrape_female
+    html = open("https://www.fakenamegenerator.com/gen-female-us-us.php")
+    doc = Nokogiri::HTML.parse(html)
+    friend_data = doc.css("div.info.content")
+    friend_data.each do |data|
+    female_friend = Hash.new
+    female_friend[:name] = friend_data.css("div.address.h3")
+    female_friend[:address] = friend_data.css("div.adr")
+    female_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
+    female_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
+    female_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
+  end
 
-def self.scrape_random
-html = open("https://www.fakenamegenerator.com/gen-random-us-us.php")
-doc = Nokogiri::HTML.parse(html)
-friend_data = doc.css("div.info.content")
-friend_data.each do |data|
-  random_friend = Hash.new
-  random_friend[:name] = friend_data.css("div.address.h3")
-  random_friend[:address] = friend_data.css("div.adr")
-  random_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
-  random_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
-  random_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
+  def self.scrape_random
+    html = open("https://www.fakenamegenerator.com/gen-random-us-us.php")
+    doc = Nokogiri::HTML.parse(html)
+    friend_data = doc.css("div.info.content")
+    friend_data.each do |data|
+    random_friend = Hash.new
+    random_friend[:name] = friend_data.css("div.address.h3")
+    random_friend[:address] = friend_data.css("div.adr")
+    random_friend[:phone] = friend_data.css("div.extra > dl:nth-child(5) > dd")
+    random_friend[:birthday] = friend_data.css("div.extra > dl:nth-child(8) > dd")
+    random_friend[:age] = friend_data.css("div.extra > dl:nth-child(9) > dd")
+  end
+
+
 end
-end
+  
