b882583c0297b591b3095669bb3e625d76815f7f
generous-vector-9581 <ojappah@gmail.com>
2019-11-03 23:55:42 +0000

    so close

--- lib/friend_generator/cli.rb
+++ lib/friend_generator/cli.rb
@@ -2,14 +2,16 @@ class FriendGenerator::CLI
   MALE_PATH = "http://www.fakenamegenerator.com/gen-male-us-us.php"
   FEMALE_PATH = "http://www.fakenamegenerator.com/gen-female-us-us.php"
   RANDOM_PATH = "https://www.fakenamegenerator.com/gen-random-us-us.php"
-
+@@friends_array = []
+@@make_male_friends = []
+@@make_female_friends = []
+@@make_random_friends = []
   def call
     friend_options
     make_male_friends
     make_female_friends
     make_random_friends
     friend_selection
-    display_friends
     exit
   end
 
@@ -23,21 +25,25 @@ class FriendGenerator::CLI
   end
 
   def make_male_friends
-    @friends_array = FriendGenerator::Friend.scrape_index_page(MALE_PATH)
-    self.create_from_collection(@friends_array)
+    @@make_male_friends << FriendGenerator::Friend.scrape_index_page(MALE_PATH)
+  #  self.create_from_collection(@friends_array)
+    @@friends_array << @@make_male_friends
   end
 
   def make_female_friends
-  @friends_array = FriendGenerator::Friend.scrape_index_page(FEMALE_PATH)
-  FriendGenerator::Friend.create_from_collection(@friends_array)
-end
+    @@make_female_friends << FriendGenerator::Friend.scrape_index_page(FEMALE_PATH)
+  #  self.create_from_collection(@friends_array)
+  @@friends_array << @@make_female_friends
+  end
 
 def make_random_friends
-  @friends_array = FriendGenerator::Friend.scrape_index_page(RANDOM_PATH)
-  FriendGenerator::Friend.create_from_collection(@friends_array)
+  @@make_random_friends << FriendGenerator::Friend.scrape_index_page(RANDOM_PATH)
+#  self.create_from_collection(@friends_array)
+  @@friends_array << @@make_random_friends
 end
   def friend_selection
-    Friend.all.each do |friend|
+
+    @@friends_array.each do |friend|
     input = nil
     while input != "exit"
       puts "Please enter a number from the options above to generate a new friend or type my friends to see a list of your current friends. Type 'exit' to leave the app."
@@ -45,7 +51,7 @@ end
       case input
         when "1"
           puts "Congratulations! You have a new friend name #{friend.name.upcase}. He is #{friend.age} years old. Type 'my friends' to see more details about your new friend."
-            create
+
         when "2"
           puts "Congratulations! You have a new friend name #{friend.name.upcase}. She is #{friend.age} years old. Type 'my friends' to see more details about your new friend."
 
@@ -54,7 +60,7 @@ end
 
         when "my friends"
           puts "Here is a list of all your new friends."
-          @@all
+
         else
           puts "That is not a valid option. Please try again. Select from the options given below."
         friend_options
@@ -63,7 +69,8 @@ end
           puts "Thank you for using Friend Generator. Come back soon to make new friends."
         end
     end
-  end
 
+
+end
 end
 end
--- lib/friend_generator/friend.rb
+++ lib/friend_generator/friend.rb
@@ -1,35 +1,45 @@
 class FriendGenerator::Friend
 
-  attr_accessor :name, :address, :phone, :birthday, :age
 @@all = []
-friend = []
-def initialize(friend_hash)
-friend_hash.each do |attribute, value|
-  self.send"#{attribute}=", value
-end
-end
+
+  attr_accessor :name, :address, :phone, :birthday, :age
+
+
+def initialize(name, address, phone, birthday, age)
+  #  FriendGenerator::Friend.all.each do |object|
+    @name = name
+    @address = address
+    @phone = phone
+    @birthday = birthday
+    @age = birthday
+
+#self.all.split("\n").each do |name, address,phone,birthday,age|
+#friend_hash.split("\n").each_with_index do |attribute, value|
+#self.send"#{attribute}=", value
 @@all << self
 end
+
 public
-def create_from_collection(friends_array)
+#def create_from_collection(friends_array)
   #friends_array.each do |friend_hash|
-    FriendGenerator::Friend.new(friends_array)
-  end
+#    FriendGenerator::Friend.new(friends_array)
+#  end
 
 def self.all
   @@all
 end
 public
-def scrape_index_page(index_page)
+def self.scrape_index_page(index_page)
   friends_array = []
     index_page = Nokogiri::HTML(open(index_page))
     friend = index_page.css("div.info .content")
     friend.css(".friend-data a")
-        friend_name = friend.css('div.address.h3').text
-        friend_address = friend.css('div.adr').text
-        friend_phone = friend.css('div.extra > dl:nth-child(5) > dd').text
-        friend_birthday = friend.css('div.extra > dl:nth-child(8) > dd').text
-        friend_age = friend.css('div.extra > dl:nth-child(9) > dd').text
-
+        name = friend.css('div.address > h3').text
+        address = friend.css('div.adr').text
+        phone = friend.css('div.extra > dl:nth-child(5) > dd').text
+        birthday = friend.css('div.extra > dl:nth-child(8) > dd').text
+        age = friend.css('div.extra > dl:nth-child(9) > dd').text
+        FriendGenerator::Friend.new(name, address, phone, birthday, age)
         #friends_array << {name: friend_name, address: friend_address, phone: friend_phone, birthday: friend_birthday, age: friend_age}
       end
+end
