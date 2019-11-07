3cd2525b63059bb498144c4f6dbfc4ee0cb6b364
generous-vector-9581 <ojappah@gmail.com>
2019-11-07 22:34:03 +0000

    DRY code

--- lib/friend_generator/cli.rb
+++ lib/friend_generator/cli.rb
@@ -2,15 +2,14 @@ class FriendGenerator::CLI
   MALE_PATH = "http://www.fakenamegenerator.com/gen-male-us-us.php"
   FEMALE_PATH = "http://www.fakenamegenerator.com/gen-female-us-us.php"
   RANDOM_PATH = "https://www.fakenamegenerator.com/gen-random-us-us.php"
-@@friends_array = []
-@@male_friends = []
-@@female_friends = []
-@@random_friends = []
+  @@friends_array = []
+  @@male_friends = []
+  @@female_friends = []
+  @@random_friends = []
   def call
     menu
     create_friends
     friend_selection
-    goodbye
   end
 
   def menu
@@ -38,53 +37,48 @@ class FriendGenerator::CLI
 
     input = nil
     while input != "goodbye"
-    puts  "Type 'Friends' to see more details on all of your created friends. Type 'goodbye' to leave the app."
-      input = gets.strip.downcase
-      case input
-        when "1"
-          puts "Congratulations! You have a new friend name #{@@male_friends[0].name.upcase}. He was born on #{@@male_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
+        puts  "Type 'friends' to see more details on all of your created friends. Type 'goodbye' to leave the app."
+        input = gets.strip.downcase
+        case input
+            when "1"
+                puts "Congratulations! You have a new friend name #{@@male_friends[0].name.upcase}. He was born on #{@@male_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
 
-        when "2"
-          puts "Congratulations! You have a new friend name #{@@female_friends[0].name}. She was born on #{@@male_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
+            when "2"
+                puts "Congratulations! You have a new friend name #{@@female_friends[0].name}. She was born on #{@@male_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
 
-        when "3"
-          puts "Congratulations! You have a new randomly generated friend name #{@@random_friends[0].name.upcase}. Your new friend was born on #{@@random_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
+            when "3"
+                puts "Congratulations! You have a new randomly generated friend name #{@@random_friends[0].name.upcase}. Your new friend was born on #{@@random_friends[0].birthday}. Type 'my friends' to see more details about your new friend."
 
-        when "my friends"
-          @@friends_array.flatten.each do |friends|
-          puts "Here you can find more details about your newly created friends."
-          puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address}\n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
+            when "friends"
+                @@friends_array.flatten.each do |friends|
+                puts "Here you can find more details about your newly created friends."
+                puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address}\n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
+                end
+
+            when "4"
+                @@male_friends.flatten.each do |friends|
+                puts "Here is a list of all your male friends."
+                puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
+                end
+
+            when "5"
+                @@female_friends.flatten.each do |friends|
+                puts "Here is a list of all your female friends."
+                puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
+                end
+
+            when "6"
+                @@random_friends.flatten.each do |friends|
+                puts "Here is a list of all your randomly created friends."
+                puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
+                end
+
+            when "goodbye"
+                puts "Thank you for using Friend Generator. Come back soon to create new friends."
+            else
+                puts "That is not a valid option. Please try again. Select from the options given below."
+                menu
+            end
         end
-
-      when "4"
-        @@male_friends.flatten.each do |friends|
-        puts "Here is a list of all your male friends."
-        puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
-      end
-
-    when "5"
-      @@female_friends.flatten.each do |friends|
-      puts "Here is a list of all your female friends."
-      puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
     end
-
-  when "6"
-    @@random_friends.flatten.each do |friends|
-    puts "Here is a list of all your randomly created friends."
-    puts "Name:#{friends.name.upcase}, \nAddress:#{friends.address} \n Phone: #{friends.phone}, \n DOB: #{friends.birthday}, \n Age: #{friends.age}."
-  end
-
-when "goodbye"
-          goodbye
-      else
-          puts "That is not a valid option. Please try again. Select from the options given below."
-          menu
-        end
-
-        def goodbye
-          puts "Thank you for using Friend Generator. Come back soon to create new friends."
-        exit
-      end
-end
-end
 end
--- lib/friend_generator/friend.rb
+++ lib/friend_generator/friend.rb
@@ -1,48 +1,29 @@
 class FriendGenerator::Friend
 
-@@all = []
-
-  attr_accessor :name, :address, :phone, :birthday, :age
+    attr_accessor :name, :address, :phone, :birthday, :age
 
 
-def initialize(name, address, phone, birthday, age)
-  #  FriendGenerator::Friend.all.each do |object|
-    @name = name
-    @address = address
-    @phone = phone
-    @birthday = birthday
-    @age = age
+    def initialize(name, address, phone, birthday, age)
 
-#self.all.split("\n").each do |name, address,phone,birthday,age|
-#friend_hash.split("\n").each_with_index do |attribute, value|
-#self.send"#{attribute}=", value
-@@all << self
-end
+      @name = name
+      @address = address
+      @phone = phone
+      @birthday = birthday
+      @age = age
+    end
 
-public
-#def create_from_collection(friends_array)
-  #friends_array.each do |friend_hash|
-#    FriendGenerator::Friend.new(friends_array)
-#  end
 
-def self.all
-  @@all
-end
-public
-def self.scrape_index_page(index_page)
-  friends_array = []
-    index_page = Nokogiri::HTML(open(index_page))
-    friend = index_page.css("div.info .content")
-    friend.css(".friend-data a")
-        name = friend.css('div.address > h3').text
-        address = friend.css('div.adr').text
-        phone = friend.css('div.extra > dl:nth-child(5) > dd').text
-        birthday = friend.css('div.extra > dl:nth-child(8) > dd').text
-        age = friend.css('div.extra > dl:nth-child(9) > dd').text
-        FriendGenerator::Friend.new(name, address, phone, birthday, age)
-        #friends_array << {name: friend_name, address: friend_address, phone: friend_phone, birthday: friend_birthday, age: friend_age}
-      end
-      def self.reset_all
-        @@all.clear
-      end
+    def self.scrape_index_page(index_page)
+        friends_array = []
+        index_page = Nokogiri::HTML(open(index_page))
+        friend = index_page.css("div.info .content")
+        friend.css(".friend-data a")
+          name = friend.css('div.address > h3').text
+          address = friend.css('div.adr').text
+          phone = friend.css('div.extra > dl:nth-child(5) > dd').text
+          birthday = friend.css('div.extra > dl:nth-child(8) > dd').text
+          age = friend.css('div.extra > dl:nth-child(9) > dd').text
+          FriendGenerator::Friend.new(name, address, phone, birthday, age)
+
+    end
 end
